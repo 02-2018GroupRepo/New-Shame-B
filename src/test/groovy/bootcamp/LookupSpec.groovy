d99b23f7384fc3339c14bbfa12b2c556ac99494d
Noelle <noelle.r.irvin@gmail.com>
2018-03-28 11:28:24 -0400

    First Commit

---
+++ src/test/groovy/bootcamp/LookupSpec.groovy
@@ -0,0 +1,14 @@
+package bootcamp
+
+import spock.lang.Specification
+
+class LookupSpec extends Specification {
+	
+	def "Test Scenario"(){
+		given: "This is test scenario"
+		
+		when: "I run the test with Maven"
+		
+		then: "These should be included"
+	}
+}
